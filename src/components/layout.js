958d2a90d445521f6011b4e7aed484ca612b39de
n8hopp <hopper26@up.edu>
2023-04-17 08:47:31 -0700

    Make animated bar look good

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,12 +4,14 @@ import '../styles/layout.scss'
 const Layout = ({ pageTitle, children }) => {
   return (
     <div>
-      <nav>
-        <ul class="primary-nav">
-          <li class="nav-item is-active"><Link to="/">Home</Link></li>
-          <li class="nav-item"><Link to="/experience">Experience</Link></li>
-        </ul>
-      </nav>
+      <nav class="nav-box-border">
+        <div class="nav-logo">Logo</div>
+        <ul class="PrimaryNav with-indicator">
+            <li class="Nav-item"><a href="#">projects</a></li>
+            <li class="Nav-item is-active"><a href="#">experience</a></li>
+            <li class="Nav-item"><a href="#">contact</a></li>
+          </ul>
+      </nav><br></br>
       <main>
         <h1>{pageTitle}</h1>
         {children}
